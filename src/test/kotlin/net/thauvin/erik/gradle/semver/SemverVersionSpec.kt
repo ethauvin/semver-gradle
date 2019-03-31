bf1e1494bc0abcca6f0a87d2e5a4b9cc028d85b4
Erik C. Thauvin <erik@thauvin.net>
2019-03-31 10:44:08 -0700

    Updated copyright.

--- src/test/kotlin/net/thauvin/erik/gradle/semver/SemverVersionSpec.kt
+++ src/test/kotlin/net/thauvin/erik/gradle/semver/SemverVersionSpec.kt
@@ -1,7 +1,7 @@
 /*
  * SemverVersionSpec.kt
  *
- * Copyright (c) 2018, Erik C. Thauvin (erik@thauvin.net)
+ * Copyright (c) 2018-2019, Erik C. Thauvin (erik@thauvin.net)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
