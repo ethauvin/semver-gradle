1f7861e314c52eed962067f4a7a106164a2d1ce9
Erik C. Thauvin <erik@thauvin.net>
2019-07-30 15:45:26 -0700

    Made versions an extra object.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.21.0"
     id("com.gradle.build-scan") version "2.3"
     id("com.gradle.plugin-publish") version "0.10.1"
-    id("io.gitlab.arturbosch.detekt") version "1.0.0-RC14" // don't update until kotlin 1.3.401 is supported by Gradle
+    id("io.gitlab.arturbosch.detekt") version "1.0.0-RC14" // don't update until kotlin 1.3.41 is supported by Gradle
     id("org.jmailen.kotlinter") version "1.26.0" // don't update until kotlin 1.3.41 supported by Gradle
     id("org.sonarqube") version "2.7.1"
     kotlin("jvm") version "1.3.31"
@@ -17,13 +17,14 @@ plugins {
 version = "1.0.4"
 group = "net.thauvin.erik.gradle"
 
-var github = "https://github.com/ethauvin/semver-gradle"
-var packageName = "net.thauvin.erik.gradle.semver"
-
-object Versions {
+object VersionInfo {
     const val kotlin = "1.3.31"
     const val spek = "2.0.5"
 }
+val versions: VersionInfo by extra { VersionInfo }
+
+val github = "https://github.com/ethauvin/semver-gradle"
+val packageName = "net.thauvin.erik.gradle.semver"
 
 repositories {
     jcenter()
@@ -32,13 +33,13 @@ repositories {
 dependencies {
     implementation(gradleApi())
 
-    testImplementation(kotlin("reflect", version = Versions.kotlin))
-    testImplementation(kotlin("test", version = Versions.kotlin))
+    testImplementation(kotlin("reflect", version = versions.kotlin))
+    testImplementation(kotlin("test", version = versions.kotlin))
     testImplementation(gradleTestKit())
 
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:${Versions.spek}")
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:${Versions.spek}")
-    implementation(kotlin("stdlib-jdk8", version = Versions.kotlin))
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:${versions.spek}")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:${versions.spek}")
+    implementation(kotlin("stdlib-jdk8", version = versions.kotlin))
 }
 
 tasks {
@@ -116,12 +117,4 @@ pluginBundle {
         groupId = project.group.toString()
         artifactId = project.name
     }
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
