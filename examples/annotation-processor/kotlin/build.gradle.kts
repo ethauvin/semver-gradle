e1ac095df05017a8524d57d14951692be70c2331
Erik C. Thauvin <erik@thauvin.net>
2025-05-30 13:36:42 -0700

    Bump Kotlin to version 2.1.21

--- examples/annotation-processor/kotlin/build.gradle.kts
+++ examples/annotation-processor/kotlin/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("application")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("net.thauvin.erik.gradle.semver") version "1.0.4"
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
     kotlin("kapt") version "1.9.25"
 }
 
--- examples/kotlin/build.gradle.kts
+++ examples/kotlin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("application")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("net.thauvin.erik.gradle.semver") version "1.0.4"
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.1.21"
 }
 
 // ./gradlew
